50fd9207ff432a999d5dea06bfe51c799b6923da
veronique demers <>
2024-10-31 12:46:32 +0000

    build script

---
+++ init.sh
@@ -0,0 +1,4 @@
+# cd home/vdemers/github/vllm/
+# pyenv activate vllm-expt
+# nvidia-smi
+# CUDA_VISIBLE_DEVICES=1 vllm serve /data/chatterina/models/granite-8b-instruct-preview-4k-r240917a --dtype auto --port 8001
